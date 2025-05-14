d23620b38db2036752f5b1709bfaf203586a9e64
Honeyhavvi2012 <18so0072@odl.silveroaks.co.in>
2025-05-14 10:45:55 +0530

    Add files via upload

---
+++ And or operater.py
@@ -0,0 +1,22 @@
+a= 10 
+b= 12
+c= 0
+
+if a and b and c:
+    print("All the numbers have bolean value as True")
+else:
+    print("Atleast one number has boolen value as False")
+
+a= 10
+b= -10
+c= 0
+
+if a> 0 or b> 0:
+    print("Either of the number is greter than 0")
+else:
+    print("No munber is greter than 0")
+
+if b> 0 or b> 0:
+    print("Either of the number is greter than 0")
+else:
+    print("No number is greter than 0")
