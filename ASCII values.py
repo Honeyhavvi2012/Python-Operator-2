eb33c63e295cb4efc3915d1b2f2113cec81105a8
Honeyhavvi2012 <18so0072@odl.silveroaks.co.in>
2025-05-19 19:20:14 +0530

    Add files via upload

---
+++ ASCII values.py
@@ -0,0 +1,10 @@
+
+
+print("ASCII values of uppercase letters:")
+for ch in range(ord('A'), ord('Z') + 1):
+    print(f"{chr(ch)}: {ch}")
+
+print("\nASCII values of lowercase letters:")
+
+for ch in range(ord('a'), ord('z') + 1):
+    print(f"{chr(ch)}: {ch}")
