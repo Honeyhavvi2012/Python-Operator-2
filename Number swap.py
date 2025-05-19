05689e4198c79430a1f2a48152f56bfd9bc5a119
Honeyhavvi2012 <18so0072@odl.silveroaks.co.in>
2025-05-19 19:16:58 +0530

    Add files via upload

---
+++ Number swap.py
@@ -0,0 +1,11 @@
+a = 10  
+b = 20  
+c = 30  
+
+print("Before swapping:")
+print("a =", a, "b =", b, "c =", c)
+
+a, b, c = b, c, a
+
+print("\nAfter swapping to correct positions:")
+print("a =", a, "b =", b, "c =", c)
